69723f4e9c0b04d73cb768bc87e6383dcd213f09
Josh Cox <josh@webhosting.coop>
2017-07-02 23:36:28 -0500

    debugs

--- start.sh
+++ start.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+echo pwd
+pwd
+echo ls
+ls -lh
+echo ls conf
+ls /conf
+which oauth2_proxy
+
+
 if [ ! -f /conf/oauth2_proxy.cfg ]
   then
     oauth2_proxy -config /conf/oauth2_proxy.cfg -upstream $UPSTREAM $OATH2_PROXY_ARGS
