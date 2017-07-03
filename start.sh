767f31065c6801d225f89f3365d8daf2486831c9
Josh Cox <josh@webhosting.coop>
2017-07-03 09:50:27 -0500

    email

--- start.sh
+++ start.sh
@@ -1,17 +1,16 @@
 #!/bin/sh
 
-echo pwd
-pwd
-echo ls
-ls -lh
-echo ls conf
-ls /conf
 which oauth2_proxy
+OAUTH2_PROXY=$(which oauth2_proxy)
 
+# if unset or null make it '*'
+EMAIL_DOMAIN="${EMAIL_DOMAIN:-'*'}"
+
+OATH_CMDLINE="-upstream $UPSTREAM email-domain=$EMAIL_DOMAIN $OATH2_PROXY_ARGS"
 
 if [ -f /conf/oauth2_proxy.cfg ]
   then
-    oauth2_proxy -config /conf/oauth2_proxy.cfg -upstream $UPSTREAM $OATH2_PROXY_ARGS
-else
-    oauth2_proxy -upstream $UPSTREAM $OATH2_PROXY_ARGS
+    OATH_CMDLINE="-config /conf/oauth2_proxy.cfg $OATH_CMDLINE"
 fi
+
+$OAUTH2_PROXY $OAUTH_CMDLINE
