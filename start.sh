f8d842ad560b6ff3e891c3dd9b7332bb5f367f65
Josh Cox <josh@webhosting.coop>
2017-07-03 14:14:52 -0500

    start

--- start.sh
+++ start.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-which oauth2_proxy
-export OAUTH2_PROXY=$(which oauth2_proxy)
-
 # if unset or null make it '*'
 EMAIL_DOMAIN="${EMAIL_DOMAIN:-'*'}"
 
-export OATH_CMDLINE="-upstream $UPSTREAM email-domain=$EMAIL_DOMAIN $OATH2_PROXY_ARGS"
+export OAUTH_CMDLINE="-upstream $UPSTREAM email-domain=$EMAIL_DOMAIN $OATH2_PROXY_ARGS"
 
 if [ -f /conf/oauth2_proxy.cfg ]
   then
     export OATH_CMDLINE="-config /conf/oauth2_proxy.cfg $OATH_CMDLINE"
 fi
 
-echo "$(OAUTH2_PROXY) $(OAUTH_CMDLINE)"
+echo "cmdline is $OAUTH_CMDLINE"
 echo 'starting .....'
-$OAUTH2_PROXY $OAUTH_CMDLINE
+
+oauth2_proxy $OAUTH_CMDLINE
